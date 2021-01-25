02925311588e6be9a2ace47bc5adc74f5acd0920
Harold Parra <gc.harold.parra@gmail.com>
2021-01-25 17:03:05 +0000

    Add basic validation to User attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
-  #validates :username,
-    #presence: true,
-    #length: { maximum: 12 },
-    #format: { with: /[A-Za-z]/, message: "only allows letters" }
+  validates :username,
+    presence: true,
+    length: { maximum: 12 },
+    format: { with: /[A-Za-z]/, message: "only allows letters" }
 
-  #validates :email,
-    #presence: true,
-    #format: { with: /\S*@\S*\.\S*/, message: "only allows valid email addresses"}
+  validates :email,
+    presence: true,
+    format: { with: /\S*@\S*\.\S*/, message: "only allows valid email addresses"}
 
-  #validates :password,
-    #presence: true,
-    #length: { in: 6..12}
+  validates :password,
+    presence: true,
+    length: { in: 6..12 }
 end
